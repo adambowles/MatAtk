30800948567b4ffa0f8109427c90ec0a099e824a
Adam Bowles <bowlesa@aston.ac.uk>
2015-04-09 09:05:02 +0100

    initial pseudo code

--- main.m
+++ main.m
@@ -1,1 +1,12 @@
-
+%{
+  loop through images in the image directory and generate their measures
+  PSEUDOCODE
+%}
+directory = 'what/ever/directory';
+filetype = 'jpg';
+file_mask = strcat(strcat(directory, '.'), filetype);
+files = dir(file_mask);
+for file = files'
+    img = imread(file.name);
+    % Do some stuff, like process(img);
+end
